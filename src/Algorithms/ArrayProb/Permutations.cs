e777820a5c64701af9a5cd508751e73d8bf1d7ff
Hung Cao <hungqcao@gmail.com>
2019-05-10 13:49:28 -0700

    add permutation

---
+++ src/Algorithms/ArrayProb/Permutations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.ArrayProb
+{
+    public class Permutations
+    {
+        public static IList<IList<int>> GetAllPermutations(IList<int> array)
+        {
+            var result = new List<IList<int>>();
+            void Permutate(IList<int> currentArray, int startIndex)
+            {
+                for (int i = startIndex; i < array.Count; i++)
+                {
+                    var tmpArray = new List<int>(currentArray);
+                    tmpArray.Add(array[i]);
+                    result.Add(tmpArray);
+                    Permutate(tmpArray, i + 1);
+                }
+            }
+
+            Permutate(new List<int>(), 0);
+            return result;
+        }
+    }
+}
---
+++ src/UnitTestProject/ArrayProb/PermutationsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.ArrayProb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.ArrayProb.Tests
+{
+    [TestClass]
+    public class PermutationsTests
+    {
+        [TestMethod]
+        public void GetAllPermutationsTest()
+        {
+            ArrayProb.Permutations.GetAllPermutations(new List<int>() { 1, 2, 3, 4});
+            Assert.AreEqual("", "");
+        }
+    }
+}
